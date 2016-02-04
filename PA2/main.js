410e1a47cdaabd44b279bf35c5b951f8054f93c7
Henry Conklin <henrywconklin@gmail.com>
2016-02-03 20:51:19 -0500

    Read from command line argument file

--- PA2/main.js
+++ PA2/main.js
@@ -1,9 +1,23 @@
-var parser= require("./grammar").parser;
-var lexer = parser.lexer;
 
+fs = require('fs');
+fs.readFile(process.argv[2], 'ASCII', function (err, data) {
+    if (err) {
+	console.log(err);
+	return;
+    }
+    
+    var parser= require("./grammar").parser;
+    var lexer = parser.lexer;
 
-lexer.setInput('123 hello 3 a334\nanother');
-while(!lexer.done) {
-    token = lexer.lex();
-    console.log((lexer.yylineno+1) + "\n" + token + "\n" + lexer.yytext);    
-}
+    /// NOTE: lexer increments line number before newline,
+    ///       assignment wants after
+
+    lexer.setInput(data);
+    while(!lexer.done) {
+	token = lexer.lex();
+	// Lexer gives an end of file token: 1
+	if (token != 1)
+	    console.log((lexer.yylineno+1) + "\n" + token + "\n" + lexer.yytext); 
+    }
+
+});
