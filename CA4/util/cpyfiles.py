c7671bebaf7c2ec3b0e70afa2ccb8e17c924d2e9
SebastianJay <jgs3cd@virginia.edu>
2016-03-06 16:52:02 -0500

    added copy util

---
+++ CA4/util/cpyfiles.py
@@ -0,0 +1,10 @@
+#Jay - for copying files from GitHub repo to VM share folder
+from shutil import copytree, rmtree
+from os.path import exists
+
+src = 'C:/Users/Student/Documents/GitHub/LDI-Cool/CA4/test'
+dst = 'C:/VM_share/ca4'
+
+if exists(dst):
+    rmtree(dst)
+copytree(src, dst)
